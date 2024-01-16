f1700ffca2615328a50cfb37f021e609eecd80db
Samridhii1212 <115480641+Samridhii1212@users.noreply.github.com>
2024-01-16 16:56:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Tenzies from './tenzies';
+
+function App() {
+  return (
+    <div className="App">
+      <Tenzies/>
+    
+    </div>
+  );
+}
+
+export default App;
